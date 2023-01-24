3db8f9149a31109897474712fc57d4e3d0587da8
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2023-01-24 17:41:47 +0000

    Working player body using IK systems

--- LC Honours Project/Assets/Scripts/AnimateHandOnInput.cs
+++ LC Honours Project/Assets/Scripts/AnimateHandOnInput.cs
@@ -9,6 +9,9 @@ public class AnimateHandOnInput : MonoBehaviour
     public InputActionProperty gripAnimationAction;
 
     public Animator handAnimator;
+
+    public bool isLeft;
+    public bool isRight;
     void Start()
     {
         
@@ -16,10 +19,18 @@ public class AnimateHandOnInput : MonoBehaviour
 
     void Update()
     {
-        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
+        if(isLeft){
+            float leftTriggerValue = pinchAnimationAction.action.ReadValue<float>();
+            handAnimator.SetFloat("LeftTrigger", leftTriggerValue);
 
-        float gripValue = gripAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Grip", gripValue);
+            float leftGripValue = gripAnimationAction.action.ReadValue<float>();
+            handAnimator.SetFloat("LeftGrip", leftGripValue);
+        } else if(isRight){
+            float rightTriggerValue = pinchAnimationAction.action.ReadValue<float>();
+            handAnimator.SetFloat("RightTrigger", rightTriggerValue);
+
+            float rightGripValue = gripAnimationAction.action.ReadValue<float>();
+            handAnimator.SetFloat("RightGrip", rightGripValue);
+        }
     }
 }
